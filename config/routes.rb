cf8a64f3ccaab75ebab1068565ef3339cb7d3b82
伊藤 一希 <kzkio114@gmail.com>
2024-05-11 18:14:07 +0900

    google認証

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'google_login_api/callback'
   get 'static_pages/before_login'
   get 'static_pages/after_login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
 
   root 'static_pages#before_login'
   get '/after_login', to: 'static_pages#after_login'
+  post '/google_login_api/callback', to: 'google_login_api#callback'
 end
